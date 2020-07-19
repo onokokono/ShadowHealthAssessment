17fb2f6b0d36affbc18d0d9b176216db896ceee9
Onon G <onokokono@gmail.com>
2020-07-19 16:33:31 -0700

    initial commit :tada:

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  const barSOne = document.getElementById('one');
+  const barSTwo = document.getElementById('two');
+  const barSThree = document.getElementById('three');
+
+  barSOne.classList.add('loading');
+  barSTwo.classList.add('loading');
+  barSThree.classList.add('loading');
+};
